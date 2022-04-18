6819af379e8af462142215b4e0c3e9931260612d
Mateus Moutinho Queiroz <61179749+mateusmoutinho@users.noreply.github.com>
2022-04-18 00:19:28 -0300

    implementing fragment sistem

--- py_component.py
+++ py_component.py
@@ -7,6 +7,7 @@ class PyComponent(list):
 
     def __init__(self,tag:str=None,props={},category="normal") -> None:
         self.tag = tag 
+        self.fragment = False  if self.tag else True 
         self.props = props
         self.category = category
 
@@ -28,9 +29,13 @@ class PyComponent(list):
     
     def _render_childs(self,ident:int,acumulated_ident:int)->str:
         rendered_childs = ''
+        
+        print(acumulated_ident)
+        acumulated_ident = acumulated_ident + ident if not self.fragment else acumulated_ident
         for child in self:
             if hasattr(child,'render'):
-                rendered_childs+=  child.render(ident,acumulated_ident+ident) 
+
+                rendered_childs+=  child.render(ident,acumulated_ident) 
             else:
                 rendered_childs+=str(child)
     
@@ -39,8 +44,10 @@ class PyComponent(list):
 
     def render(self, ident=4,acumulated_ident=0)->str:
         props = self._render_props()
+
         childs = self._render_childs(ident,acumulated_ident)
         ident_text = self.create_ident_text(acumulated_ident)
+        
         if not self.tag:
             return childs
             
